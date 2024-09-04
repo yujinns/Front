2630337a74bbcb4e9913dec49851ed7c054ecd19
Gyulo94 <qkrcjfrb112@gmail.com>
2024-09-04 17:02:52 +0900

    input 컴포넌트 작성

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,85 @@
+import { ChangeEvent, KeyboardEvent, forwardRef } from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import './style.css';
+
+// interface: Input 컴포넌트 //
+interface Props {
+  label: string;
+  type: "text" | "password";
+  error: boolean;
+  placeholder: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+
+  icon?: "eye-light-off-icon" | "eye-light-on-icon" | "expand-right-light-icon";
+  onButtonClick?: () => void;
+
+  message?: string;
+
+  onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+// component: Input 컴포넌트 //
+const Input = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+  // state: properties //
+  const { label, placeholder, error, type, value, icon, message } = props;
+  const { onChange, onButtonClick, onKeyDown } = props;
+
+  // event handler: input 키 이벤트 처리 함수 //
+  const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (!onKeyDown) return;
+    onKeyDown(e);
+  };
+
+  // render: Input Box 렌더링 //
+  return (
+    <div className="input">
+      <div className="input-label">{label}</div>
+      <div
+        className={error ? "input-container-error" : "input-container"}
+      >
+        <input
+          className="input"
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          ref={ref}
+          onChange={onChange}
+          onKeyDown={onKeyDownHandler}
+        />
+        {onButtonClick !== undefined && (
+          <div className="icon-button" onClick={onButtonClick}>
+            {icon !== undefined && <div className={`icon ${icon}`}></div>}
+          </div>
+        )}
+      </div>
+      {message !== undefined && (
+        <div className="inputbox-message">{message}</div>
+      )}
+    </div>
+  );
+});
+
+interface searchProps {
+  onButtonClick?: () => void;
+  className?: string;
+  size: number;
+}
+
+const SearchInput = (props: searchProps) => {
+
+  const {onButtonClick, className, size} = props;
+  
+  return (
+    <div id="searchInput">
+      <input type="text"className={className} placeholder="검색어를 입력하세요." />
+      <div className="icon-box">
+        <IoSearchSharp size={size} onClick={onButtonClick}/>
+      </div>
+    </div>
+  );
+}
+
+
+export { Input, SearchInput };
+
