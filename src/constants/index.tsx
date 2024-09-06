c4c44b536e7b93774fd750e59643349c8fc2734f
Gyulo94 <qkrcjfrb112@gmail.com>
2024-09-06 13:32:54 +0900

    test

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,5 +1,5 @@
-const MAIN_PATH = () => '/'
-const AUTH_PATH = () => '/auth'
+const MAIN_PATH = () => "/";
+const AUTH_PATH = () => "/auth";
+const test = () => "/test";
 
-
-export { AUTH_PATH, MAIN_PATH }
+export { AUTH_PATH, MAIN_PATH };
